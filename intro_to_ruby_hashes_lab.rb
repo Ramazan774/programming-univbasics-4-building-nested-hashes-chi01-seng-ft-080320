0666e8cef71adcc4be6f39c93f2dfcb1f5fb12da
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-20 19:11:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,12 @@ return base_hash  # keyword "return." Nice and neat.
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
+ base_hash = {
+   :railroads => {
+     :pieces => "4",
+     :rent_in_dollars => 
+   }
+ } # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
